90d6681c77d90239339ba205b95f69ca891aec6e
Tommaso Bratto <tommaso.bratto@credemtel.it>
2026-09-09 11:39:09 +0200

    refactored store display to use idiomatic React

--- packages/explore/src/StorePicker.tsx
+++ packages/explore/src/StorePicker.tsx
@@ -3,17 +3,44 @@ import data from './data/db.json';
 import Button from './components/Button';
 import { src, srcset } from './js/utils';
 
+function StoreDisplay({ store }: { store: { id: string; name: string; street: string; city: string; image: string; } }) {
+  if (!store) return <></>
+
+  return (
+    <div className="e_StorePicker_content">
+      <img
+        className="e_StorePicker_image"
+        src={src(store.image, 200)}
+        srcSet={srcset(store.image, [200, 400])}
+        width="200"
+        height="200"
+      />
+      <p className="e_StorePicker_address">
+        {store.name}
+        <br />
+        {store.street}
+        <br />
+        {store.city}
+      </p>
+    </div>
+  );
+}
+
 const StorePicker: React.FC = () => {
   const [currentStore, setCurrentStore] = React.useState('');
   const ref = React.useRef<HTMLDialogElement>();
+
+  const currentStoreDisplay = React.useMemo(() => {
+    return data.stores.find(store => store.id === currentStore);
+  }, [data, currentStore]);
 
   const openDialog = () => {
     ref.current.showModal();
   };
 
-  const selectShop = (e: React.MouseEvent) => {
+  const selectShop = (e: React.MouseEvent, shopId: string) => {
     const shop = e.currentTarget.getAttribute('data-id');
-    setCurrentStore(e.currentTarget.previousElementSibling.innerHTML);
+    setCurrentStore(shopId);
     window.dispatchEvent(
       new CustomEvent('selected-shop', {
         detail: { shop },
@@ -25,7 +52,7 @@ const StorePicker: React.FC = () => {
   return (
     <div className="e_StorePicker">
       <div className="e_StorePicker_control" data-boundary="explore">
-        <div className="e_StorePicker_selected" dangerouslySetInnerHTML={{ __html: currentStore }} />
+        <div className="e_StorePicker_selected">{<StoreDisplay store={currentStoreDisplay} />}</div>
         <Button className="e_StorePicker_choose" type="button" onClick={openDialog}>
           choose a store
         </Button>
@@ -36,31 +63,16 @@ const StorePicker: React.FC = () => {
           <ul className="e_StorePicker_list">
             {data.stores.map((s, i) => (
               <li className="e_StorePicker_entry" key={i}>
-                <div className="e_StorePicker_content">
-                  <img
-                    className="e_StorePicker_image"
-                    src={src(s.image, 200)}
-                    srcSet={srcset(s.image, [200, 400])}
-                    width="200"
-                    height="200"
-                  />
-                  <p className="e_StorePicker_address">
-                    {s.name}
-                    <br />
-                    {s.street}
-                    <br />
-                    {s.city}
-                  </p>
-                </div>
-                <Button className="e_StorePicker_select" type="button" dataId={s.id} onClick={selectShop}>
+                <StoreDisplay store={s}/>
+                <Button className="e_StorePicker_select" type="button" dataId={s.id} onClick={(e) => selectShop(e, s.id)}>
                   select
                 </Button>
               </li>
             ))}
-          </ul>
-        </div>
-      </dialog>
+        </ul>
     </div>
+      </dialog >
+    </div >
   );
 };
 
